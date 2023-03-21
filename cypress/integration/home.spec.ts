8c40db022727c6fd4edd24558e56e1f9e12895e5
EricGit <hi@ericgit.me>
2023-03-21 16:06:44 +0500

    [ADDED] test for the course features

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -5,4 +5,11 @@ describe("Home Page", () => {
       "Testing Next.js Applications with Cypress"
     )
   })
+
+  it.only("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
+  })
 })
